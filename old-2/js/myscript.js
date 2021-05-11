9cc46e557f8e497bfa250f7e5e2b1930c8bf92d0
Codie Stephens-Evans <codie18@gmail.com>
2021-05-11 15:48:14 +0000

    tabular

---
+++ old-2/js/myscript.js
@@ -0,0 +1,44 @@
+// $("p").click(function(){
+//     $("this").slideToggle("slow");
+// });
+
+
+// $("button").mouseenter(function(){
+//     $(this).removeClass("make-red").addClass("make-blue");
+// });
+
+// $("button").mouseleave(function(){
+//     $(this).removeClass("make-blue").addClass("make-red");
+// });
+
+
+
+// $(document).ready(function() {
+//     $(".box").on("click", function() {
+//     	/**
+//     	 * When we click on an element that has
+//     	 * a 'box' class, this event will be fired.
+//     	 */
+//     	var classNames = $(this).attr("class").split(" ");
+//         $("." + classNames[1]).css("background-color", "red");
+//     });
+// });
+
+$(document).ready(function() {
+    $(".box").on('click',function() {
+        var classNames = $(this).attr("class").split(" ");
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if ($(this).css("background-color") == "rgb(255,0,0)") {
+            // if this object is already red, turn it black
+                $("." + className).css("background-color", "#000");
+            } else {
+                // else turn all elements with a box class black
+                // and then change the color of all elements
+                // to red
+                $("." + boxClass).css("background-color", "#000");
+                $("." + className).css("background-color", "rgb(255,0,0)");
+            }
+    });
+})
+
